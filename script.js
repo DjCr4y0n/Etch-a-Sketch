e7218ce5c53396fd44c2b69b4363de1d408cbef5
DjCr4y0n <cray.kox1234@gmail.com>
2023-10-13 12:23:13 +0200

    add a mesh and a bunch of grids, align to the center my mesh

---
+++ script.js
@@ -0,0 +1,8 @@
+var mesh = document.querySelector('.mesh');
+for (i=0;i<16;i++)
+{
+    let grid = document.createElement('div');
+    grid.className = 'grid';
+    console.log(mesh);
+    mesh.appendChild(grid);
+}
