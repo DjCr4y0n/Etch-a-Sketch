4cff4a75041fedb01daf20ce85ddf08a9cde9ba1
DjCr4y0n <cray.kox1234@gmail.com>
2023-10-13 13:02:04 +0200

    fill mesh with divs class ggrid and wrap them in container called mesh, also make mesh 128x128 [px] and grids set to 96x96 [px]

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ for (i=0;i<16;i++)
 {
     let grid = document.createElement('div');
     grid.className = 'grid';
+    grid.style.width = "96px";
+    grid.style.height = "96px";
+    grid.style.backgroundColor = "white";
     console.log(mesh);
     mesh.appendChild(grid);
 }
