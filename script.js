5e096202570b263cad702058539ffedcf6dbed8f
DjCr4y0n <cray.kox1234@gmail.com>
2023-10-14 21:55:51 +0200

    Add restart button, add button letting user to change color and add text letting know how many squares on the mesh are

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var mesh = document.querySelector('.mesh');
 
-var squaresAmount = prompt('Please enter number of square per side', "Min:0, Max:33");
-let squaresAmountText = document.querySelector('.p')
+var squaresAmount = prompt('Please enter number of square per side', "Min:0, Max:70");
+let squaresAmountText = document.querySelector('.squaresAmountText');
+var color = 'gray';
+function restart()
+{
+    window.location.reload();
+}
+
+function chooseColor()
+{
+    color = prompt('Choose color you want to paint with');
+    color.toString().toLowerCase();
+}
 
   
 var lengthOfSquare = 900/squaresAmount;
@@ -13,6 +24,8 @@ for (i=0;i<squaresAmount*squaresAmount;i++)
     grid.style.width = lengthOfSquare.toString()+"px";
     grid.style.height = lengthOfSquare.toString()+"px";
     grid.style.backgroundColor = "white";
-    grid.addEventListener("mouseenter", (event) => {event.target.style.backgroundColor = "gray";})
+    grid.addEventListener("mouseenter", (event) => {event.target.style.backgroundColor = color})
     mesh.appendChild(grid);
 }
+
+squaresAmountText.textContent ="Amount of squares in total: " + squaresAmount*squaresAmount;
