ed394b30f793f8b3604bd0af573987bcb90c9cc1
DjCr4y0n <cray.kox1234@gmail.com>
2023-10-14 21:24:19 +0200

    Add temporary colors to page, change mesh to 900x900 px, add prompt window asking for amount of square per side, add to script changes with sizing grids.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var mesh = document.querySelector('.mesh');
 
+var squaresAmount = prompt('Please enter number of square per side', "Min:0, Max:33");
+let squaresAmountText = document.querySelector('.p')
 
-for (i=0;i<100;i++)
+  
+var lengthOfSquare = 900/squaresAmount;
+for (i=0;i<squaresAmount*squaresAmount;i++)
 {
     let grid = document.createElement('div');
-    grid.style.flex = "initial";
+    grid.style.flex = "flex";
     grid.className = 'grid';
-    grid.style.width = "96px";
-    grid.style.height = "96px";
+    grid.style.width = lengthOfSquare.toString()+"px";
+    grid.style.height = lengthOfSquare.toString()+"px";
     grid.style.backgroundColor = "white";
     grid.addEventListener("mouseenter", (event) => {event.target.style.backgroundColor = "gray";})
-    console.log(mesh);
     mesh.appendChild(grid);
 }
